c2fb373003f6e392d82e1a40cd0dcd5ef15044d5
mostafaelsayed <test@youremail.com>
2017-07-30 08:29:38 +0200

    change in index page of cafeteria in customer area

--- CafeteriaApp/CafeteriaApp.Frontend/Areas/Customer/Cafeteria/Views/index.php
+++ CafeteriaApp/CafeteriaApp.Frontend/Areas/Customer/Cafeteria/Views/index.php
@@ -16,20 +16,20 @@ include('CafeteriaApp.Frontend/Areas/Customer/layout.php');
     <div  id="myCarousel" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
           <li data-target="#myCarousel" data-slide-to=0 class="active"></li>
-          <li ng-repeat="c in cafeterias | limitTo:1:cafeterias.length-1" data-target="#myCarousel" data-slide-to={{cafeterias.indexOf(c)}}></li>
+          <li ng-repeat="c in cafeterias.slice(1,cafeterias.length)" data-target="#myCarousel" data-slide-to={{cafeterias.indexOf(c)}}></li>
       </ol>
       <div class="carousel-inner">
         <div class="item active">
           <a ng-href="/CafeteriaApp.Frontend/Areas/Customer/Cafeteria/Views/show.php?id={{cafeterias[0].Id}}">
-            <img style="display:block;margin:auto" ng-src="/CafeteriaApp.Frontend/Scripts/CustomerTheme/images/bbig1.jpg" width="500" height="400" />
+            <img style="display:block;margin:auto" ng-src={{cafeterias[0].Image}} width="500" height="400" />
           </a>
           <div class="carousel-caption">
             <h3 ng-bind="cafeterias[0].Name"></h3>
           </div>
         </div>
-        <div ng-repeat="c in cafeterias | limitTo:1:cafeterias.length-1" class="item">
+        <div ng-repeat="c in cafeterias.slice(1,cafeterias.length)" class="item">
             <a ng-href="/CafeteriaApp.Frontend/Areas/Customer/Cafeteria/Views/show.php?id={{c.Id}}">
-              <img style="display:block;margin:auto" ng-src="/CafeteriaApp.Frontend/Scripts/CustomerTheme/images/bbig3.jpg" width="500" height="400" />
+              <img style="display:block;margin:auto" ng-src={{c.Image}} width="500" height="400" />
             </a>
             <div class="carousel-caption">
               <h3 ng-bind="c.Name"></h3>
