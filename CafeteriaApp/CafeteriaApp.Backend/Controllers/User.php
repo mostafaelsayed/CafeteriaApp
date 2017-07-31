3928d1c2a47029265a45a9eecc4b8e255e5e61a2
mohamed-nabawy <mm_h434@yahoo.com>
2017-07-31 11:36:05 +0200

    forgot modifying user controller like the other

--- CafeteriaApp/CafeteriaApp.Backend/Controllers/User.php
+++ CafeteriaApp/CafeteriaApp.Backend/Controllers/User.php
@@ -313,40 +313,4 @@ function deleteUser($conn,$id) { // cascaded delete ??
 
 
 
-if ($_SERVER['REQUEST_METHOD']=="GET") {
-  if (isset($_GET["action"]) && $_GET["action"]=="getUsers"){
-    getUsers($conn);
-  }
-  else {
-    echo "Error occured while returning Users";
-  }
-}
-
-if ($_SERVER['REQUEST_METHOD']=="POST"){
-    //decode the json data
-    $data = json_decode(file_get_contents("php://input"));
-    if (isset($data->action) && $data->action == "addUser"){
-      if ($data->Name != null){
-        addUser($conn,$data->Name);
-      }
-      else{
-        echo "name is required";
-      }
-  }
-}
-
-if ($_SERVER['REQUEST_METHOD']=="PUT"){
-    //decode the json data
-    $data = json_decode(file_get_contents("php://input"));
-    //echo $data;
-      if ($data->Name != null && $data->Id != null) {
-        //if ($data->action == "addcafeteria"){
-        editUser($conn,$data->Name,$data->Id);
-      }
-      else{
-        echo "name is required";
-      }
-  //}
-}
-
  ?>
