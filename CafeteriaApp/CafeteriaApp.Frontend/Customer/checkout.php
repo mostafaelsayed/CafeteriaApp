a1600682467a8c19561d82606282a3074181e5d9
mostafaelsayed <test@youremail.com>
2018-04-14 13:39:50 +0200

    fix layout style

--- CafeteriaApp/CafeteriaApp.Frontend/Customer/checkout.php
+++ CafeteriaApp/CafeteriaApp.Frontend/Customer/checkout.php
@@ -1,19 +1,16 @@
 <?php
   require(__DIR__.'/../layout.php');
-    validatePageAccess([2]);
+  validatePageAccess([2]);
 ?>
 
 <head>
-
   <title>Order Checkout</title>
-
   <link href="../css/alertify.bootstrap.css" rel="stylesheet">
   <link href="../css/alertify.core.css" rel="stylesheet">
   <link href="../css/alertify.default.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="../css/map.css">
-  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCeWPPmdZODmjq3pLXNQVEzPm6X2eZm5dY"></script>
-<script src="../js/calculate_distance_given_longitude_and_latitude.js"></script>
+  <script src="../js/calculate_distance_given_longitude_and_latitude.js"></script>
 </head>
 
 <div ng-controller="OrderCheckout" ng-init="phoneDisabled=true" class="container" style="align-content: center;text-align: center">
@@ -22,16 +19,14 @@
   Complete Order #<span>{{orderId}}</span> info.
   </h1>
 
-  <form novalidate name="myForm" action="../../CafeteriaApp.Backend/Requests/Order.php" method="post" style="text-align:left;color:white;background-color:#A52A2A;border-radius:20px;">
-    <input type="hidden" class="form-control"   ng-model="selectedMethod.id" name="selectedMethodId"/>
-    <input type="hidden" class="form-control"  ng-model="selectedType.id" name="orderType"/>
-    <input type="hidden" class="form-control"  ng-model="total" name="total"/>
+  <form novalidate name="myForm" action="../../CafeteriaApp.Backend/Requests/Order.php" method="post" style="text-align:center;color:white;background-color:#A52A2A;border-radius:20px;">
+    <input type="hidden" class="form-control" ng-model="selectedMethod.id" name="selectedMethodId"/>
+    <input type="hidden" class="form-control" ng-model="selectedType.id" name="orderType"/>
+    <input type="hidden" class="form-control" ng-model="total" name="total"/>
     <br>
     <div style="width: 550px;margin:auto;">
       <div>Get Order by</div>
-      <select class="form-control" ng-model="selectedType" ng-options="type.name for type in orderTypes" ng-change="changeType()"></select>
-
-      <!-- <div><button ng-click="changeType()">Confirm Type</button></div> -->
+      <select class="form-control" style="width: auto;" ng-model="selectedType" ng-options="type.name for type in orderTypes" ng-change="changeType()"></select>
 
       <br><br><br>
 
@@ -47,7 +42,7 @@
       <div>Payment Method</div>
 
       <div>
-        <select class="form-control" name="method" ng-model="selectedMethod" ng-change="changePaymentMethod()" ng-options="method.name for method in paymentMethods" required />
+        <select class="form-control" style="width: auto;" name="method" ng-model="selectedMethod" ng-change="changePaymentMethod()" ng-options="method.name for method in paymentMethods" required />
        	</select>
 
         <span ng-show="myForm.$submitted && myForm.method.$invalid" ng-cloak>The Payment Method is required.</span>
@@ -56,11 +51,10 @@
     </div>
 
     <br />
-   
-      <div class="map-wrapper">
-        <h4 style="text-align:center;">
-          Click on the map above to Change Your Delivery Location
-        </h4>
+    <div class="map-wrapper">
+      <h4 style="text-align:center;">
+        Click on the map above to Change Your Delivery Location
+      </h4>
       <div id="map"></div>
       <br/>
       <br/>
@@ -74,22 +68,29 @@
     </div><!--End Wrapper-->
 
     <br><br>
+
     <div class="form-group text-center">
-      <input type="submit" class="btn btn-primary" ng-show="selectedMethod.id == 1"   name="next" value="Next" />
 
-      <a  type="button" class="form-control" ng-show="selectedMethod.id == 2" href="../Templates/credit-card-payment.php">Next</a>
+      <input type="submit" class="btn btn-primary" ng-show="selectedMethod.id == 1" name="next" value="Next" />
 
+      <a class="btn btn-primary" ng-show="selectedMethod.id == 2" href="../Templates/credit-card-payment.php">Next</a>
 
       <a class="btn btn-primary" ng-show="selectedMethod.id == 3" ng-click="confirmOrder()">Submit</a>
 
       <a class="btn btn-primary" ng-click="discardOrder()">Discard Order</a>
+
     </div>
     <br><br>
   </form>
-
-
 </div>
+
 <script src="../js/alertify.js"></script>
 <script src="../js/checkout.js"></script>
-<script src="../js/bootstrap.min.js"></script>
+
 <?php require( __DIR__ . '/../Public/footer.php'); ?>
+
+<style type="text/css">
+  select {
+    margin: 0 auto
+  }
+</style>
--- CafeteriaApp/CafeteriaApp.Frontend/layout.php
+++ CafeteriaApp/CafeteriaApp.Frontend/layout.php
@@ -51,184 +51,276 @@ $selected_lang ='en';
     <div ng-controller="Language_Order" ng-init="languages=['English', 'Arabic'];selectedLang = 1;orderId = <?= $orderId;?>" id="myctrl">
 
       <!-- Navigation -->
-      <nav class="navbar navbar-fixed-top left-to-right w3-animate-top">
-            <div class="navbar-header" style="display:inline;!important">
-          <!--     <button class="navbar-toggle pull-left" data-toggle="collapse" data-target="#optionsNavbar" id="mybutton" >
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button> -->
-               <img id="logo" style="width:60px;height:60px;" src="/CafeteriaApp/CafeteriaApp/CafeteriaApp.Frontend/images/logo_main.png">
-            </div> 
-            <!-- left navigation -->
-            <!-- <div id="optionsNavbar" class="navbar-collapse"> -->
-             
+      <nav class="navbar navbar-fixed-top w3-animate-top">
+        <button type="button" id="dropdownBut" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
 
-              <ul id="left_ul" class="nav navbar-nav navbar-left">
-                <li>
-                  <a  href='../Public/categories.php'>Home</a>
-                </li>
-                <li>
-                  <a  href="../Public/help.php">Help</a>
-                </li>
-                <li>
-                  <a  href="ssss.php">Contact us</a>
-                </li>
-            </ul>
-          <!-- </div> -->
+        <div class="navbar-header" style="display:inline;!important">
 
-         
+          <img id="logo" style="width:60px;height:60px;" src="/CafeteriaApp/CafeteriaApp/CafeteriaApp.Frontend/images/logo_main.png">
 
-          <ul id="right_ul" >
+        </div>
+
+        <div class="collapse navbar-collapse" id="myNavbar">
+
+          <ul class="nav navbar-nav">
+
+            <li>
+
+              <a href='../Public/categories.php'>Home</a>
+
+            </li>
+
+            <li>
+
+              <a href="../Public/help.php">Help</a>
+
+            </li>
+
+            <li>
+
+              <a href="ssss.php">Contact us</a>
+
+            </li>
+
+            <li role="separator" class="divider"></li>
+
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+
             <li ng-if='orderId' id="shoppingCart" title="Show Shopping Cart Items">
-              <div id="shoppingCart-btn">
-                <img src="../images/icons/cart-layout.png" />
-              </div>
+
+              <img id="shoppingCart-btn" src="../images/icons/cart-layout.png" />
+
               <div id="shoppingCartDetails">
+
                 <h3 id="OrderItemsHeader">Order Items</h3>
-                  <h5  ng-show="orderItems.length == 0">Empty Cart !</h5>
-                  <div id="OrderContents" ng-show="orderItems.length > 0">
-                    <table id="orderTable" class="table table-bordered" >
-                      <thead>
-                        <tr>
-                          <th>Item</th>
-                          <th>Quantity</th>
-                          <th>Total Price</th>
-                          <th>Actions</th>
-                        </tr>
-                      </thead>
-                      <tbody ng-if="orderItems.length>0" ng-repeat="o in orderItems">
-                        <tr>
-                          <td ng-bind="o.Name" ></td>
-                          <td ng-bind="o.Quantity"></td>
-                          <td ng-bind="o.TotalPrice"></td>
-                          <td style="display: block;width: 100%; padding: 0px">
-                            <ul style="list-style: none; margin: 0px;padding: 0px">
-                              <li>
-                                <a title="Increase Quantity" ng-click="increaseQuantity(o)" class="btn">
-                                  <i class="fa fa-plus"></i>
-                                </a>
-                              </li>
-                              <li>
-                                <a title="Decrease Quantity" ng-click="decreaseQuantity(o)" class="btn">
-                                  <i class="fa fa-minus"></i>
-                                </a>
-                              </li>
-                              <li>
-                                <a title="Remove From Order" ng-click="deleteOrderItem(o)" style="font-weight: bold" class="btn">X</a>
-                              </li>
-                            </ul>
 
-                          </td>
+                <h5 ng-show="orderItems.length == 0">Empty Cart !</h5>
 
-                        </tr>
+                <div id="OrderContents" ng-show="orderItems.length > 0">
 
-                      </tbody>
+                  <table id="orderTable" class="table table-bordered">
 
-                    </table> 
+                    <thead>
 
-                    <div id="cart_checkout">
-                      <a class="btn checkout"  title="Check out this order" href="../Customer/checkout.php?orderId={{orderId}}" >
+                      <tr>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                        <th>Total Price</th>
+                        <th>Actions</th>
+                      </tr>
+
+                    </thead>
+
+                    <tbody ng-if="orderItems.length > 0" ng-repeat="o in orderItems">
+
+                      <tr>
+
+                        <td ng-bind="o.Name"></td>
+
+                        <td ng-bind="o.Quantity"></td>
+
+                        <td ng-bind="o.TotalPrice"></td>
+
+                        <td style="display: block;width: 100%; padding: 0px">
+
+                          <ul style="list-style: none; margin: 0px;padding: 0px">
+
+                            <li>
+
+                              <a title="Increase Quantity" ng-click="increaseQuantity(o)" class="btn">
+
+                                <i class="fa fa-plus"></i>
+
+                              </a>
+
+                            </li>
+
+                            <li>
+
+                              <a title="Decrease Quantity" ng-click="decreaseQuantity(o)" class="btn">
+
+                                <i class="fa fa-minus"></i>
+
+                              </a>
+
+                            </li>
+
+                            <li>
+
+                              <a title="Remove From Order" ng-click="deleteOrderItem(o)" style="font-weight: bold" class="btn">X</a>
+
+                            </li>
+
+                          </ul>
+
+                        </td>
+
+                      </tr>
+
+                    </tbody>
+
+                  </table> 
+
+                  <div id="cart_checkout">
+
+                    <a class="btn checkout" title="Check out this order" href="../Customer/checkout.php?orderId={{orderId}}">
+
                       Checkout <span class="glyphicon glyphicon-usd"></span>
-                      </a>
+
+                    </a>
+
                   </div>
 
-                      
                 </div>
 
+              </div>
 
-                </div>
+            </li>
 
-              </li>
+            <li id="notification" title="Show Notifications" onclick="toggleNotifications()">
 
-          <li id="notification" title="Show Notifications" onclick="toggleNotifications()">
+              <?php $length = count($_SESSION['notifications']); ?>
 
-                  <?php $length = count($_SESSION['notifications']); ?>
-                  <button id="notify-btn" type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+              <button id="notify-btn" type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
-                    <?php
-                      if ($length > 0) {
-                        echo "<img src='../images/alarm-1.png' style='width:40px; height:40px;'/>
-                        <label id='notifyLabel' style='color: blue;font-size: 2em'>{$length}</label>";
-                      }
-                      else {
-                        echo "<img src='../images/alarm.png' style='width:40px; height:40px;'/>";
-                      }
+                <?php
+                  if ($length > 0) {
+                    echo "<img src='../images/alarm-1.png' style='width:40px; height:40px;'/>
+                      <label id='notifyLabel' style='color: blue;font-size: 2em'>{$length}</label>";
+                  }
+                  else {
+                    echo "<img src='../images/alarm.png' style='width:40px; height:40px;'/>";
+                  }
 
-                      if ( !empty($_SESSION['notifications']) && is_array($_SESSION['notifications']) ) {
-                        $ul = "<ul style='color: blue;'>";
+                  if ( !empty($_SESSION['notifications']) && is_array($_SESSION['notifications']) ) {
+                    $ul = "<ul style='color: blue;'>";
 
-                        foreach ($_SESSION["notifications"] as $value) {
-                          $ul .= "<li><h4>" . $value . "</h4></li>";
-                        }
+                    foreach ($_SESSION["notifications"] as $value) {
+                      $ul .= "<li><h4>" . $value . "</h4></li>";
+                    }
 
-                        $ul .= "</ul>";
-                      }
-                    ?>
+                    $ul .= "</ul>";
+                  }
+                ?>
 
-                  </button>
-                  <div class="dropdown-menu" style="left:-100px;top:35px;width:300px;background-color: #FFC806">
+              </button>
+
+              <div class="dropdown-menu" style="left:-100px;top:35px;width:300px;background-color: #FFC806">
                 
-                    <?= isset($ul)? $ul : "<h5>No Notifications</h5>"; ?>
+                <?= isset($ul)? $ul : "<h5>No Notifications</h5>"; ?>
                    
-                  </div>
+              </div>
 
-              </li>
+            </li>
 
-              <li id="languagesDropdown">
-                <div id="languages" class="dropdown">
-                  <button class="btn language" type="button" data-toggle="dropdown"> 
-                    <span><?=$selected_lang == 'en' ? "En" : "Ar"?></span> <i class="glyphicon glyphicon-chevron-down"></i>
+            <li id="languagesDropdown" class="btn-sm">
+
+              <div class="dropdown">
+
+                <button class="btn language dropdown-toggle" type="button" data-toggle="dropdown">
+
+                  <span><?=$selected_lang == 'en' ? "En" : "Ar"?></span>
+
+                  <i class="glyphicon glyphicon-chevron-down"></i>
+
                 </button>
+
                 <ul class="dropdown-menu dropdown-langs">
-                  <li><span class="glyphicon glyphicon-ok check-mark"></span><a onclick="changeUserLang(1);" href="<?=count($_GET) > 1 ? str_replace('lang=en', 'lang=ar', $_SERVER['REQUEST_URI']) : $_SERVER['PHP_SELF'] . "?lang=ar";?>"><img src="/Shipping/images/header/Egypt-Flag.png" width="20" alt=""> Arabic</a>
+
+                  <li class="dropdown-item">
+
+                    <span class="glyphicon glyphicon-ok check-mark"></span>
+
+                    <a onclick="changeUserLang(1);" href="<?=count($_GET) > 1 ? str_replace('lang=en', 'lang=ar', $_SERVER['REQUEST_URI']) : $_SERVER['PHP_SELF'] . "?lang=ar";?>"><img src="/Shipping/images/header/Egypt-Flag.png" width="20" alt=""> Arabic
+                    </a>
+
                   </li>
-                  <li class="divider" style="margin:4px 0"></li>
-                  <li><span></span><a onclick="changeUserLang(0);"  href="<?=count($_GET) > 1 ? str_replace('lang=ar', 'lang=en', $_SERVER['REQUEST_URI']) : $_SERVER['PHP_SELF'] . "?lang=en";?>"><img src="/Shipping/images/header/United-Kingdom-flag.png" width="20" alt=""> English</a>
+
+                  <!-- <li class="divider" style="margin:4px 0"></li> -->
+
+                  <li class="dropdown-item">
+
+                    <span></span>
+
+                    <a onclick="changeUserLang(0);" href="<?=count($_GET) > 1 ? str_replace('lang=ar', 'lang=en', $_SERVER['REQUEST_URI']) : $_SERVER['PHP_SELF'] . "?lang=en";?>"><img src="/Shipping/images/header/United-Kingdom-flag.png" width="20" alt=""> English
+
+                    </a>
+
                   </li>
-              </ul>
-            </div>
-              </li>
-           <?php   if(isset($_SESSION['userId'])){ ?>
-              <li id="myProfile">
+
+                </ul>
+
+              </div>
+
+            </li>
+
+            <?php if ( isset($_SESSION['userId']) ) { ?>
+
+              <li id="myProfile" class="btn-sm">
+
                 <div class="btn-group">
+
                   <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    My Profile 
+                    My Profile
+
                     <span class="glyphicon glyphicon-user"></span>
+
                   </button>
+
                   <div class="dropdown-menu" style="left: -70px">
-                      <a class="dropdown_item" href="../Customer/favorite items.php" > My Favorites</a>
-                      <a class="dropdown_item" href="#">Change Info</a>
-                      <a class="dropdown_item" href="../Customer/change_password.php">Change Password</a>
-                      <hr>
-                      <a class="dropdown_item" href="../logout.php">Log out
-                      </a>
+
+                    <a class="dropdown_item" href="../Customer/favorite items.php"> My Favorites</a>
+
+                    <a class="dropdown_item" href="#">Change Info</a>
+
+                    <a class="dropdown_item" href="../Customer/change_password.php">Change Password</a>
+
+                    <hr>
+
+                    <a class="dropdown_item" href="../logout.php">Log out
+
+                    </a>
+
                   </div>
 
                 </div>
 
               </li>
-            
-    
-               <?php }else{ ?>
-               <li style="margin-left: 70px;">
-                <a href="../login.php" class="btn btn-info" style="font-size:2rem;padding-top:5px;" > 
+
+            <?php } else { ?>
+
+              <li style="margin-left: 70px;">
+
+                <a href="../login.php" class="btn btn-info" style="font-size: 2rem;padding-top: 5px;">
+
                   Login
+
                   <span class="glyphicon glyphicon-log-in"></span>
+
                   </a>
+
                 </li>
 
-                <?php } ?>
-            </ul>
+            <?php } ?>
 
+          </ul>
 
+        </div>
 
       </nav>
-</div>
-   <?php 
-   if(!isset($_SESSION['userId'])) {
-    echo "<h1>login <a href='../login.php'>here</a> so you can order food, rate items, add comments and more ... </h1>";
-   }
+
+    </div>
+
+    <?php 
+      if ( !isset($_SESSION['userId']) ) {
+        echo "<h1>login <a href='../login.php'>here</a> so you can order food, rate items, add comments and more ... </h1>";
+      }
     ?> 
 
     <script src="../js/jquery-3.2.1.min.js"></script>
@@ -246,6 +338,17 @@ $selected_lang ='en';
     $('#notifyLabel').html('');
     $("#notifyme").slideToggle("slow");
   }
+
+  $(document).click(function (event) {
+      var clickover = $(event.target);
+      var $navbar = $(".navbar-collapse");               
+      var _opened = $navbar.hasClass("in");
+
+      // check if it's open and we clicked outside the toggle button
+      if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
+          $navbar.collapse('hide');
+      }
+  });
 
 
   $.urlParam = function(name){
